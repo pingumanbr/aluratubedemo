402de9fb0cb656ba6a17bbe9bb4b812d064a0b69
Manoel Dantas <manoel.dantas@gmail.com>
2022-11-13 14:50:51 -0300

    Aula 05 - Ultima correcao !!!!

--- pages/src/components/RegisterVideo/index.js
+++ pages/src/components/RegisterVideo/index.js
@@ -41,9 +41,6 @@ function useForm(propsDoForm){
     }  
 };
 
-const PROJECT_URL = "https://gusozvtmmqdfrjtmojej.supabase.co";
-const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1c296dnRtbXFkZnJqdG1vamVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgzNDIyNzcsImV4cCI6MTk4MzkxODI3N30.GTjZooqbeCNs_6lz4bMstWQP40Fv7-RiKxGSOY2Ar_4"
-const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
 
 
 export default function RegisterVideo(){
@@ -89,7 +86,7 @@ export default function RegisterVideo(){
                         })
                         .then((oqueveio) => {console.log(oqueveio)})
                         .catch((error) => {console.log(error)})
-                        
+
                         setformVisivel(false);
                         formCadastro.clearForm();
                     }}>
