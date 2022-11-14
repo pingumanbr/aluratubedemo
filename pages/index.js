e2905e21fe8f3341eeceb2f8887bbd686b4aef32
Manoel Dantas <manoel.dantas@gmail.com>
2022-11-14 14:59:40 -0300

    Corrigido mais uma vez,dessa vez compila local !!!!!

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import config from "./config.json";
 import styled from "styled-components";
+import { createClient } from "@supabase/supabase-js";
 
 import React from 'react';
 import Menu from "./src/components/Menu";
 import { StyledTimeline } from "./src/components/Timeline"
-import  videoService, { getAllVideos }  from "./src/components/services/videoService";
+// import  { getAllVideos }  from "./src/components/services/videoService";
+const PROJECT_URL = "https://gusozvtmmqdfrjtmojej.supabase.co";
+const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1c296dnRtbXFkZnJqdG1vamVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgzNDIyNzcsImV4cCI6MTk4MzkxODI3N30.GTjZooqbeCNs_6lz4bMstWQP40Fv7-RiKxGSOY2Ar_4"
+const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
 
 
 function HomePage() {
-  const service = getAllVideos();
+  // const service = getAllVideos;
   const [valorDoFiltro, setValorDoFiltro] = React.useState("");
   const [playlists,setPlaylists] = React.useState({});
 
@@ -21,7 +25,8 @@ function HomePage() {
   // uma variavel seja atualizada
   React.useEffect(() => {
     console.log("Useffect");
-    service
+    supabase.from("video")
+            .select("*")
            .then((dados) => {
               console.log(dados);
               //Forma imutavel
@@ -100,7 +105,7 @@ const StyledBanner = styled.div`
         {/* <img src="banner"/> */}
         <StyledBanner bg={config.bg}/>
         <section className="user-info">
-          <Image src={`https://github.com/${config.github}.png`}/>
+          <img src={`https://github.com/${config.github}.png`}/>
           <div>
               <h2>
                 {config.name}
