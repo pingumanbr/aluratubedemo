b40b8d63147bcf6f3db8f699437211a4a8cd1869
Manoel Dantas <manoel.dantas@gmail.com>
2022-11-14 15:03:25 -0300

    Comentarios em HomePage

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { createClient } from "@supabase/supabase-js";
 import React from 'react';
 import Menu from "./src/components/Menu";
 import { StyledTimeline } from "./src/components/Timeline"
-// import  { getAllVideos }  from "./src/components/services/videoService";
+
 const PROJECT_URL = "https://gusozvtmmqdfrjtmojej.supabase.co";
 const PUBLIC_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd1c296dnRtbXFkZnJqdG1vamVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjgzNDIyNzcsImV4cCI6MTk4MzkxODI3N30.GTjZooqbeCNs_6lz4bMstWQP40Fv7-RiKxGSOY2Ar_4"
 const supabase = createClient(PROJECT_URL, PUBLIC_KEY);
@@ -25,9 +25,9 @@ function HomePage() {
   // uma variavel seja atualizada
   React.useEffect(() => {
     console.log("Useffect");
-    supabase.from("video")
-            .select("*")
-           .then((dados) => {
+    supabase.from("video").               // Tive que reinserir o codigo do videoService aqui !
+            .select("*")                  // Ao fazer o build, dava problema de export
+            .then((dados) => {
               console.log(dados);
               //Forma imutavel
               //E feito pq o React tem que receber dado novo.
